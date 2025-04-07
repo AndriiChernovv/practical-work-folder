5d0b6c8f8bc18bd246006ca5b0b7c367eb7dce98
AndriiChernovv <andrii.chernovv@gmail.com>
2025-04-07 11:46:33 +0300

    Update output.py

---
+++ python_practice_7/project_template/app/io/output.py
@@ -0,0 +1,9 @@
+
+
+def print_to_console(text):
+    """Виводить текст у консоль."""
+    pass
+
+def write_to_file(path, text):
+    """Записує текст у файл за допомогою вбудованих засобів Python."""
+    pass
